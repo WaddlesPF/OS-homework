bbf2d18dec3f0569a7f2736211560e3b8b254a45
WaddlesPF <peytonfazio@gmail.com>
2023-11-06 21:54:55 -0500

    Adding producer.cpp and consumer.cpp

---
+++ consprodproblem/consumer.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+sem_t* emptySlots;
+sem_t* fullSlots;
+sem_t mutc;
+int bsize = 5;
+
+int main(int argc, char* argv[]) {
+    std::cout << "consumer program start" << std::endl;
+    
+    // Shared Memory Setup
+    int shmid = shmget(1236, bsize, IPC_CREAT | 0666);
+    std::cout << errno << std::endl;
+    if(shmid == -1) std::cerr << "shared mem fail" << std::endl;
+    bool* table = (bool*)shmat(shmid, NULL, 0);
+    std::cout << errno << std::endl;
+
+    std::cout << "shared memory done" << std::endl;   
+
+    // Semaphore open/init
+    
+    int ret = sem_init(&mutc, 0, 1);
+    if(ret == -1) return -1;
+    std::cout << "sem_init successful" << std::endl;
+    
+    emptySlots = sem_open("/emptysem", 0);
+    if(emptySlots == SEM_FAILED) {std::cout << errno << std::endl; return -1; }
+
+    fullSlots = sem_open("/fullsem", 0);
+    if(fullSlots == SEM_FAILED) return -1; // exit if full slots failed
+    std::cout << "full slots open" << std::endl;
+
+    std::cout << "semaphore setup done (consumer)" << std::endl;
+
+    // Loop
+    do {
+        std::cout << "loop start" << std::endl;
+        sem_wait(fullSlots);
+        sem_wait(&mutc);
+        
+        int i = 0;
+        
+        while(!table[i] && i < bsize) ++i;
+        table[i] = false;
+        std::cout << i << std::endl;
+       
+        sem_post(&mutc);
+        sem_post(emptySlots);
+        std::cout << "sempost for consumer" << std::endl;
+    } while(true);
+
+
+    shmdt(table);
+    shmctl(shmid, IPC_RMID, NULL);
+  
+    return 0;
+}                                              
---
+++ consprodproblem/producer.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <errno.h>
+
+sem_t* emptySlots;
+sem_t* fullSlots;
+sem_t mutp;
+int bsize = 5;
+
+int main(int argc, char* argv[]) {
+    std::cout << "producer program start" << std::endl;
+    
+    // Shared Memory Setup
+    int shmid = shmget(1236, bsize, IPC_CREAT | 0666);
+    //std::cout << errno << std::endl;
+    if(shmid == -1) std::cerr << "shared mem fail" << std::endl;
+    bool* table = (bool*)shmat(shmid, NULL, 0);
+    std::cout << errno << std::endl;
+
+    std::cout << "shared memory done" << std::endl;
+
+    // Semaphore open/init
+    sem_unlink("/emptysem");
+    sem_unlink("/fullsem");
+    
+    int ret = sem_init(&mutp, 0, 1);
+    if(ret == -1) return -1;
+    std::cout << "sem_init successful" << std::endl;
+    
+    emptySlots = sem_open("/emptysem", O_CREAT | O_EXCL, 0644, bsize);
+    if(emptySlots == SEM_FAILED) {/*std::cout << errno << std::endl;*/ return -1; }
+
+    fullSlots = sem_open("/fullsem", O_CREAT | O_EXCL, 0644, 0);
+    if(fullSlots == SEM_FAILED) return -1; // exit if full slots failed
+    std::cout << "full slots open" << std::endl;
+
+    std::cout << "semaphore setup done (producer)" << std::endl;
+    // Producer Loop
+    do {
+        sem_wait(emptySlots);
+        sem_wait(&mutp);
+        std::cout << "sem_waits done" << std::endl;
+
+        int i = 0; 
+
+        while(table[i] && i < bsize) {++i; std::cerr << i << std::endl; }
+        table[i] = true;
+        std::cout << i << std::endl;
+        
+        sem_post(&mutp);
+        sem_post(fullSlots);
+        std::cout << "sempost for producer" << std::endl;
+
+    } while(true);
+
+
+    shmdt(table);
+    shmctl(shmid, IPC_RMID, NULL);
+  
+    return 0;
+}                                                   
+
